4791cd574547132ede07e3007c0260a63bf88861
matthewatkspk <matthewatkspk@gmail.com>
2026-05-10 19:40:00 -0600

    Add files via upload

---
+++ 1-to-50.sh
@@ -0,0 +1,201 @@
+slee#!/bin/bash
+
+echo "1"
+
+sleep 1
+
+echo "2"
+
+sleep 1
+
+echo "3"
+
+sleep 1
+
+echo "4"
+
+sleep 1
+
+echo "5"
+
+sleep 1
+
+echo "6"
+
+sleep 1
+
+echo "7"
+
+sleep 1
+
+echo "8"
+
+sleep 1
+
+echo "9"
+
+sleep 1
+
+echo "10"
+
+sleep 1
+
+echo "11"
+
+sleep 1
+
+echo "12"
+
+sleep 1
+
+echo "13"
+
+sleep 1
+
+echo "14"
+
+sleep 1
+
+echo "15"
+
+sleep 1
+
+echo "16"
+
+sleep 1
+
+echo "17"
+
+sleep 1
+
+echo "18"
+
+sleep 1
+
+echo "19"
+
+sleep 1
+
+echo "20"
+
+sleep 1
+
+echo "21"
+
+sleep 1
+
+echo "22"
+
+sleep 1
+
+echo "23"
+
+sleep 1
+
+echo "24"
+
+sleep 1
+
+echo "25"
+
+sleep 1
+
+echo "26"
+
+sleep 1
+
+echo "27"
+
+sleep 1
+
+echo "28"
+
+sleep 1
+
+echo "29"
+
+sleep 1
+
+echo "30"
+
+sleep 1
+
+echo "31"
+
+sleep 1
+
+echo "32"
+
+sleep 1
+
+echo "33"
+
+sleep 1
+
+echo "34"
+
+sleep 1
+
+echo "35"
+
+sleep 1
+
+echo "36"
+
+sleep 1
+
+echo "37"
+
+sleep 1
+
+echo "38"
+
+sleep 1
+
+echo "39"
+
+sleep 1
+
+echo "40"
+
+sleep 1
+
+echo "41"
+
+sleep 1
+
+echo "42"
+
+sleep 1
+
+echo "43"
+
+sleep 1
+
+echo "44"
+
+sleep 1
+
+echo "45"
+
+sleep 1
+
+echo "46"
+
+sleep 1
+
+echo "47"
+
+sleep 1
+
+echo "48"
+
+sleep 1
+
+echo "49"
+
+sleep 1
+
+echo "50"
+
+sleep 1
